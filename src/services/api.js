eedc81bcff5c61339090913372ce3762434c73cc
NosoaB17 <ngsn176@gmail.com>
2024-10-18 13:45:33 +0700

    Merge localhost

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000/";
+// const API_URL = "http://127.0.0.1:5000/";
+const API_URL = "http://vercel-server-three-chi.vercel.app/";
 
 const api = axios.create({
   baseURL: API_URL,
